4d11a9cc2743cb1800962af7d069b8de7e9f730e
Fabitheguy003 <fabienmukungu@gmail.com>
2023-03-15 05:54:24 -0400

    add create, update and delete  thought for thoughtConttollers.js

--- Develop/controllers/thoughtController.js
+++ Develop/controllers/thoughtController.js
@@ -30,3 +30,63 @@ res.status(400).json(err);
 }
 },
 };
+
+ // create thought
+ createThought({ body }, res) {
+    Thought.create(body)
+    .then(thought => {
+        User.findOneAndUpdate(
+            { _id: body.userId },
+            { $push: { thoughts: thought._id } },
+            { new: true }
+        )
+        .then(thought => {
+            if (!thought) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(thought);
+        })
+        .catch(err => res.json(err));
+    })
+    .catch(err => res.status(400).json(err));
+},
+
+// update thought by id
+updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate(
+        { _id: params.id },
+        body,
+        { new: true }
+    )
+    .then(thought => {
+        if (!thought) {
+            res.status(404).json({ message: 'No thought found with this id' });
+            return;
+        }
+        res.json(thought);
+    })
+    .catch(err => res.status(400).json(err));
+},
+
+ //delete thought by id
+ deleteThought({ params }, res) {
+    // delete the thought
+    Thought.findOneAndDelete({ _id: params.id })
+    .then(thought => {
+        if (!thought) {
+            res.status(404).json({ message: 'No thought found with this id'});
+            return;
+        }
+        // delete the reference to deleted thought in user's thought array
+        User.findOneAndUpdate(
+            { username: thought.username },
+            { $pull: { thoughts: params.id } }
+        )
+        .then(() => {
+            res.json({message: 'Successfully deleted the thought'});
+        })
+        .catch(err => res.status(500).json(err));
+    })
+    .catch(err => res.status(500).json(err));
+},
