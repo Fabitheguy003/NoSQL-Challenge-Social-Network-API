117605d9eb1f96b21670b426a1d44e10345a4b6e
Fabitheguy003 <fabienmukungu@gmail.com>
2023-03-15 06:57:57 -0400

    index.js and User.js with required code

---
+++ Develop/models/User.js
@@ -0,0 +1,14 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+  username: { type: String, required: true, trim: true, unique: true },
+  email: { type: String, required: true, unique: true, match: [/.+@.+\..+/] },
+  thoughts: [{ type: Schema.Types.ObjectId, ref: 'Thought' }],
+  friends: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+});
+
+userSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+module.exports = model('User', userSchema);
